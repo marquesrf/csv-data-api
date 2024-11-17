1b09b4ee2007ee7d02d5d472fd0fd67ed1967e16
Rafael Marques <marquesrf@protonmail.com>
2024-11-17 11:17:59 -0300

    Handle cors errors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const dt_ext1Route = require("./routes/dt-ext1");
 const dt_ext2Route = require("./routes/dt-ext2");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 // Rotas
 app.use("/api/get_soma_concentrado", dt_ext1Route);
 app.use("/api/get_soma_materia_prima", dt_ext2Route);
